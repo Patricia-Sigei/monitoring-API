bbf5a0c504e296e9f80c84b1d417d1f272606ca9
Patricia-Sigei <pattychela97@gmail.com>
2026-08-03 13:22:22 +0300

    improves folder structure

--- app.js
+++ app.js
@@ -3,5 +3,14 @@ const express = require('express');
 const app = express();
 
 
+app.get('/', (req, res) => {
+    res.json({ message: 'monitoring project is running 🚀' });
+});
+
+// app.use('/api', require('./routes'));
+
+app.use((req, res) => {
+    res.status(404).json({ error: 'Route not found' });
+});
 
 module.exports= app
