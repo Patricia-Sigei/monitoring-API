aa9f16fa8479634f40c5510904d8fe527b3c0f12
Patricia-Sigei <pattychela97@gmail.com>
2026-08-05 13:12:11 +0300

    imports routes on app.js

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const express = require('express');
+const express = require("express");
+
+import healthRoutes from "./routes/health.routes.js";
+import metricsRoutes from "./routes/metrics.routes.js";
+import chaosRoutes from "./routes/chaos.routes.js";
 
 const app = express();
+const router = express.Router();
 
-
-app.get('/', (req, res) => {
-    res.json({ message: 'monitoring project is running 🚀' });
+app.get("/", (req, res) => {
+  res.json({ message: "monitoring project is running 🚀" });
 });
 
-// app.use('/api', require('./routes'));
+app.use("/health", healthRoutes);
+app.use("/metrics", metricsRoutes);
+app.use("/chaos", chaosRoutes);
 
 app.use((req, res) => {
-    res.status(404).json({ error: 'Route not found' });
+  res.status(404).json({ error: "Route not found" });
 });
 
-module.exports= app
+module.exports = app;
